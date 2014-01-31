f7b677421a75b38438199f8c06ccbb9186aad114
Brandon <brantauber@Brandons-MacBook-Pro-2.local>
2014-01-31 09:59:28 -0500

    Initial Commit

---
+++ Quiz3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz3
+//
+//  Created by Brandon on 1/31/14.
+//  Copyright (c) 2014 Brandon Tauber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QuizAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QuizAppDelegate class]));
+    }
+}
